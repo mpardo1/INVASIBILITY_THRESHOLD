e72ebaa27577629bcd473a5eed9118b8754aedbc
Marta Pardo <pardoaraujomarta92@gmail.com>
2022-12-12 18:56:14 +0100

    discrete parameter space search

--- hanski_mod.R
+++ hanski_mod.R
@@ -26,12 +26,28 @@ hanski_DS <- function(N,end_time, ic, param) {
   return(p)
 }
 
-N_p <- 4
+N_p <- 1
 init <- runif(N_p,0,1)
 # init <- c(0.2,0)
 end_t <- 50
-cc <- 0.7
-ee <- 0.5
+cc <- 1
+ee <- 9
 ee <- ee-1
 out <- hanski_DS(N_p,end_t,init, c(cc,ee))
-out
+
+# Loop to see the paramater space biologically meaningfull
+p_vec <- seq(0,1,0.1)
+c_vec  <- seq(0,10,0.1)
+e_vec  <- seq(0,10,0.1)
+l <- length(p_vec)
+out_df <- data.frame(c = 0, e =0, p = 0, sig = 0)
+for(i in (1:length(c_vec))){
+  for(j in (1:length(c_vec))){
+    f <- function(init){hanski_DS(N_p,end_t,init, c(c_vec[i],e_vec[j]))}
+    p_out <- sapply(p_vec,f)
+    vec_sign <- ifelse(colSums(p_out<0) >0,1,0)
+    df <- data.frame(c = rep(c_vec[i],l), e = rep(e_vec[j],l), p = p_vec, sig =  vec_sign)
+    out_df <- rbind(out_df, df)
+  }
+}
+
