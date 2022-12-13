b0cea049bc4d5e3ca02d510ddec5a5fba1e9fc5d
Marta Pardo <pardoaraujomarta92@gmail.com>
2022-12-13 16:34:03 +0100

    antes de añadir datos

--- hanski_mod.R
+++ hanski_mod.R
@@ -13,6 +13,7 @@ library("ggplot2")
 
 ##### FUNCTIONS #####
 
+###### DISCRETE #######
 ##### Hanski: system of equations #####
 
 hanski_DS <- function(N,end_time, ic, param) {
@@ -37,8 +38,8 @@ out <- hanski_DS(N_p,end_t,init, c(cc,ee))
 
 # Loop to see the paramater space biologically meaningfull
 p_vec <- seq(0,1,0.1)
-c_vec  <- seq(0,10,0.1)
-e_vec  <- seq(0,10,0.1)
+c_vec  <- seq(0.01,1,0.005)
+e_vec  <- seq(0.01,1,0.005)
 l <- length(p_vec)
 out_df <- data.frame(c = 0, e =0, p = 0, sig = 0)
 for(i in (1:length(c_vec))){
@@ -51,3 +52,60 @@ for(i in (1:length(c_vec))){
   }
 }
 
+df_filt <- out_df[which(out_df$sig == 0),]
+ggplot(df_filt) +
+  geom_point(aes(c,e, color = p), size = 0.5)
+
+#----------------------------------------------------------------------------#
+###### CONTINUOUS #######
+
+library("tidyverse")
+library("parallel")
+library("deSolve")
+library("ggplot2")
+
+##### FUNCTIONS #####
+
+##### 1.SIR: system of equations #####
+
+hanski_DS <- function(t, y, parameters) {
+  with(as.list(c(y, parameters)),{
+    dy <- c()
+    for(i in c(1:dime)){
+      dy[i] <- col*(1 - y[i]) - ext*y[i]    
+      }
+    list(dy)
+  })
+}
+
+##### 2.integrate the system #####
+
+int <- function(N, c, e, init_p, end_time){
+  # create vector of parameters for ode function:
+  parameters <- list(
+    dime = N,
+    col = c,
+    ext = e)
+  
+  # time steps for integration:
+  times = seq(0,end_time, 0.1)
+  
+  # initial values:
+  pops <- c(init_p)
+  
+  # run integration:
+  z <- ode(pops, times, hanski_DS, parameters)
+  return(z)
+}
+
+
+# Example of integration
+dim <- 2
+c <- 0.2
+e <- 0.6
+eq <- c/(c + e)
+out <- as.data.frame(int(dim, c, e, c(0.1,0.2), 10))
+colnames(out) <- c("time", "p1", "p2")
+ggplot(out) +
+  geom_line(aes(time,p1)) + 
+  geom_hline(yintercept = eq, linetype = "dashed", color = "red")
