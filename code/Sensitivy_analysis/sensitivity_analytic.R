e6fb440e3c9a46d03dae8996d24c0d2baf0063ce
mpardo1 <pardoaraujomarta92@gmail.com>
2023-04-26 18:51:57 +0200

    derivs mal

--- code/Sensitivy_analysis/sensitivity_analytic.R
+++ code/Sensitivy_analysis/sensitivity_analytic.R
@@ -26,6 +26,33 @@ Quad_func <- function(cte, tmin, tmax, temp){
   return(outp)
 }
 
+### Incorporating rain and human density:
+h_f <- function(hum, rain){
+  # Constants: 
+  erat = 0.5
+  e0 = 1.5
+  evar = 0.05
+  eopt = 8
+  efac = 0.01
+  edens = 0.01
+  
+  
+  hatch <- (1-erat)*(((1+e0)*exp(-evar*(rain-eopt)^2))/(exp(-evar*(rain - eopt)^2) + e0)) +
+    erat*(edens/(edens + exp(-efac*hum)))
+  return(hatch)
+}
+
+# R0 function by temperature:
+R0_func_alb <- function(rain,hum,Te){
+  a <- a_f_alb(Te)
+  f <- TFD_f_alb(Te)
+  deltaa <- 1/lf_f_alb(Te)
+  probla <- pEA_f_alb(Te)
+  h <- h_f(hum,rain)
+  deltE = 0.1
+  R0 <- sqrt(f*(a/deltaa)*probla*(h*(h+deltE)))
+  return(R0)
+}
 # Main functions 
 Briere_df <- function(cte, tmin, tmax, temp){
   outp <- cte*(2*temp - tmin)*(tmax - temp)^(1/2) - ((1/2)*cte*(temp^2-tmin*temp)*(tmax-temp)^(-(1/2)))
@@ -48,6 +75,8 @@ pEA_f_alb <- function(temp){Quad_func(0.00361,9.04,39.33,temp)} # Survival proba
 MDR_f_alb <- function(temp){Briere_func(0.0000638,8.6,39.66,temp)} # Mosquito Development Rate
 lf_f_alb <- function(temp){Quad_func(1.43,13.41,31.51,temp)} # Adult life span
 
+#-------------------------------------------------------------------#
+# Check if the numerical derivatives are working
 vec <- seq(13,30,0.1)
 briere_num <- grad(function(temp){Briere_func(0.000193,10.25,38.32,temp)}, vec)
 briere_fun <- sapply(vec, function(temp){Briere_func(0.000193,10.25,38.32,temp)} )
@@ -70,4 +99,44 @@ plot_quad <- ggplot(df_plot_quad) +
   geom_point(aes(vec, value, color=variable, shape = variable))
 
 ggarrange(plot_brier, plot_quad)
+#-------------------------------------------------------------------#
+a_df_alb <- function(temp){Briere_df(0.000193,10.25,38.32,temp)} # Biting rate
+TFD_df_alb <- function(temp){Briere_df(0.0488,8.02,35.65,temp)} # Fecundity
+pEA_df_alb <- function(temp){Quad_df(0.00361,9.04,39.33,temp)} # Survival probability Egg-Adult
+MDR_df_alb <- function(temp){Briere_df(0.0000638,8.6,39.66,temp)} # Mosquito Development Rate
+lf_df_alb <- function(temp){Quad_df(1.43,13.41,31.51,temp)} # Adult life span
 
+# Derivate of R0
+R0_dfunc_alb <- function(rain,hum,Te){
+  a <- a_f_alb(Te)
+  f <- TFD_f_alb(Te)
+  deltaa <- 1/lf_f_alb(Te)
+  probla <- pEA_f_alb(Te)
+  h <- h_f(hum,rain)
+  deltE = 0.1
+  R0 <- f*(a/deltaa)*probla*(h*(h+deltE))
+  dffT <- TFD_df_alb(Te)
+  dfaT <- a_df_alb(Te)
+  dfdeltaAT <- 1/lf_df_alb(Te)
+  dfplaT <- pEA_df_alb(Te)
+  dffR0 <- (1/2)*(R0)^(-1/2)*(a*h*probla)/(deltaa*(h+deltE))*dffT
+  dfaR0 <- (1/2)*(R0)^(-1/2)*(f*h*probla)/(deltaa*(h+deltE))*dfaT
+  dfdeltAR0 <- -(1/2)*(R0)^(-1/2)*(f*a*h*probla)/((deltaa^2)*(h+deltE))*dfdeltaAT
+  dfpLAR0 <- (1/2)*(R0)^(-1/2)*(a*h*f)/(deltaa*(h+deltE))*dfplaT
+  
+  dfR0 <- dffR0 + dfaR0 + dfdeltAR0 + dfpLAR0
+  return(dfR0)
+}
+
+hum_cte <- 500
+rain_cte <- 8
+vec <- seq(13,30,0.1)
+R0df_num <- grad(function(x){R0_func_alb(rain_cte,hum_cte,x)}, vec)
+R0df_ana <- sapply(vec, function(x){R0_dfunc_alb(rain_cte,hum_cte,x)} )
+
+df_R0df <- data.frame(vec, R0df_num,R0df_ana)
+df_R0df <- reshape2::melt(df_R0df, id.vars = "vec")
+plot_quad <- ggplot(df_R0df) +
+  geom_point(aes(vec, value, color=variable, shape = variable)) +
+  ylim(c(-10,10))
+plot_quad
