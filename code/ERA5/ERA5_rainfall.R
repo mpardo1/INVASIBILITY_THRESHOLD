db6d59460c52fedeb0b7afc2ac7ddf11c01dfc6d
Marta Pardo <pardoaraujomarta92@gmail.com>
2023-05-05 12:42:09 +0200

    japonicus data

--- code/ERA5/ERA5_rainfall.R
+++ code/ERA5/ERA5_rainfall.R
@@ -13,17 +13,17 @@ library(ncdf4)
 # You need to create a file with the url and API key in (you need to log in first):https://cds.climate.copernicus.eu/api-how-to
 #-----------------------CLUSTER--------------------------#
 # # Run this the first time in cluster after comment it to dont run it
-conda_create("reticulate")
-conda_install("r-reticulate","cdsapi", pip=TRUE)#import python CDS-API
-# indicate that we want to use a specific condaenv
-use_condaenv("reticulate")
-cdsapi <- import("cdsapi")
+# conda_create("reticulate")
+# conda_install("r-reticulate","cdsapi", pip=TRUE)#import python CDS-API
+# # indicate that we want to use a specific condaenv
+# use_condaenv("reticulate")
+# cdsapi <- import("cdsapi")
 #-----------------------------------------------------#
 #-------------------LOCAL----------------------------#
 # install the CDS API
-# conda_install("r-reticulate","cdsapi", pip=TRUE)#import python CDS-API
-# 
-# cdsapi <- import("cdsapi")
+conda_install("r-reticulate","cdsapi", pip=TRUE)#import python CDS-API
+
+cdsapi <- import("cdsapi")
 #-----------------------------------------------------#
 #for this step there must exist the file .cdsapirc
 server = cdsapi$Client() #start the connection
