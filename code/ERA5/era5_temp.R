70b13e4e30bd3b53dc53f7ff148bf99bc453b016
Marta Pardo <pardoaraujomarta92@gmail.com>
2023-08-30 18:11:18 +0200

    Feb

--- code/ERA5/era5_temp.R
+++ code/ERA5/era5_temp.R
@@ -96,7 +96,7 @@ agg_daily <- function(i){
 }
 
 # Select month for extraction climate --------------------------------------
-month_s <- "January"
+month_s <- "February"
 nc_raster <- rast_temp(substr(month_s,1,3))
 plot(nc_raster[[2]])
 
