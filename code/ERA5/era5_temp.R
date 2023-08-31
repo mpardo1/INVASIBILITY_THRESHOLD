669440fb6908574c7633ed4b00fc2ac85649eca5
Marta Pardo <pardoaraujomarta92@gmail.com>
2023-08-31 10:10:42 +0200

    October

--- code/ERA5/era5_temp.R
+++ code/ERA5/era5_temp.R
@@ -99,7 +99,7 @@ agg_daily <- function(i){
 }
 
 # Select month for extraction climate --------------------------------------
-month_s <- "October"
+month_s <- "November"
 nc_raster <- rast_temp(substr(month_s,1,3))
 plot(nc_raster[[6]])
 
@@ -107,7 +107,7 @@ time_info <- time(nc_raster)
 
 # Paralelize code --------------------------------------------------
 num_cores = 1
-climat_each_muni <- mclapply(seq(1,4*31,4), 
+climat_each_muni <- mclapply(seq(1,4*30,4), 
                              agg_daily, 
                              mc.cores = num_cores)
 climat_each_muni <- setDT(do.call(cbind, climat_each_muni))
