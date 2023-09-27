561ade793298cbf6320e2e2486c1b0f603acf36d
Marta Pardo <pardoaraujomarta92@gmail.com>
2023-09-27 17:59:51 +0200

    europe maps

--- code/Europe/read_output.R
+++ code/Europe/read_output.R
@@ -1,5 +1,5 @@
 library(terra)
-rast_list <- readRDS("/home/marta/INVASIBILITY_THRESHOLD/data/ERA5/Europe/monthly_aggregated_raster.Rds")
+rast_list <- readRDS("~/INVASIBILITY_THRESHOLD/data/ERA5/Europe/monthly_aggregated_raster.Rds")
 raster_stack <- terra::stack(raster_list)
 # Save the raster stack to a new file
-terra::writeRaster(raster_stack, "/home/marta/INVASIBILITY_THRESHOLD/data/ERA5/Europe/monthly_aggregated_raster.tif", overwrite = TRUE)
+terra::writeRaster(raster_stack, "~/INVASIBILITY_THRESHOLD/data/ERA5/Europe/monthly_aggregated_raster.tif", overwrite = TRUE)
